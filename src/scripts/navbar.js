e8b4418149e170337237af4d28f568c688bf2a96
Luke A <73725429+LukeDaNuke456@users.noreply.github.com>
2025-05-13 13:36:26 -0500

    moving files over to public and formatting code. (#12)

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,15 +1,18 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("#navbar-list a");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    const link = document.querySelector(`#nav-${entry.target.id}`);
-    if (entry.isIntersecting) {
-      navLinks.forEach((link) => link.classList.remove("active-link"));
-      link?.classList.add("active-link");
-    }
-  });
-}, { threshold: 0.6 });
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const link = document.querySelector(`#nav-${entry.target.id}`);
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => link.classList.remove("active-link"));
+        link?.classList.add("active-link");
+      }
+    });
+  },
+  { threshold: 0.6 },
+);
 
 sections.forEach((section) => {
   observer.observe(section);
@@ -24,7 +27,7 @@ hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
 
-menuLinks.forEach(link => {
+menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     mobileMenu.classList.add("hidden");
     hamburger.classList.remove("active");
